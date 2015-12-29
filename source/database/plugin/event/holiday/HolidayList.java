9172c01d57bb9ec433ea8da62083387926b0c326
Maximilian Peters <uvdyr@student.kit.edu>
2015-12-29 13:34:52 +0100

    update

--- source/database/plugin/event/holiday/HolidayList.java
+++ source/database/plugin/event/holiday/HolidayList.java
@@ -5,67 +5,75 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import database.main.date.Date;
 import database.plugin.Instance;
 import database.plugin.event.EventList;
 
 public class HolidayList extends EventList {
+	URL				url;
+	URLConnection	connection;
+	List<String>	lines	= new ArrayList<String>();
+
 	@Override public void add(Map<String, String> parameter) throws IOException {
-		if (new Date(parameter.get("date")).isPast()) {
+		url = new URL("http://www.schulferien.org/Feiertage/Feiertage_Baden_Wuerttemberg.html");
+		if (new Date(parameter.get("date")).isPast() && testURL()) {
 			getHolidays();
 		}
-		else {
+		if ((new Date(parameter.get("date")).isPast() && !testURL()) || !contains(parameter)) {
 			sortedAdd(new Holiday(parameter));
 		}
 	}
 
-	private void getHolidays() throws IOException {
-		int lineCounter = 0;
+	private boolean testURL() {
+		try {
+			connection = url.openConnection();
+			return true;
+		}
+		catch (Throwable e) {
+			return false;
+		}
+	}
+
+	public void getHolidays() throws IOException {
 		String line;
-		String[] lines;
 		Map<String, String> map;
-		URL url = new URL("http://www.schulferien.org/Feiertage/Feiertage_Baden_Wuerttemberg.html");
-		URLConnection conn = url.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		while ((line = in.readLine()) != null) {
-			lineCounter++;
+		if (lines.isEmpty()) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			while ((line = in.readLine()) != null) {
+				lines.add(line);
+			}
+			in.close();
 		}
-		lines = new String[lineCounter];
-		lineCounter = 0;
-		in.close();
-		conn = url.openConnection();
-		in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		while ((line = in.readLine()) != null) {
-			lines[lineCounter++] = line;
-		}
-		in.close();
-		for (int i = 0; i < lines.length; i++) {
-			if (lines[i].matches(".*<a href=\"/Feiertage/feiertag_.*.html\" class=\"dash\">.*")) {
+		for (int i = 0; i < lines.size(); i++) {
+			if (lines.get(i).matches(".*<a href=\"/Feiertage/feiertag_.*.html\" class=\"dash\">.*")) {
 				map = new HashMap<String, String>();
-				String temp = lines[i + 5].replace(" ", "");
+				String temp = lines.get(i + 5).replace(" ", "");
 				String date = temp.substring(0, temp.lastIndexOf(","));
-				temp = lines[i + 1].replace("&ouml;", "ö").replace(":", " ");
+				temp = lines.get(i + 1).replace("&ouml;", "ö").replace(":", " ");
 				String name = temp.substring(14, temp.length() - 1);
+				Date newDate = new Date(date);
 				map.put("name", name);
 				map.put("date", date);
-				if (getList().isEmpty() && new Date(map.get("date")).compareTo(Date.getCurrentDate()) >= 0) {
+				if (getList().isEmpty() && !newDate.isPast()) {
 					sortedAdd(new Holiday(map));
 				}
 				else {
 					boolean contains = false;
 					for (Instance instance : getList()) {
 						Holiday holiday = (Holiday) instance;
-						if (holiday.getName().equals(map.get("name"))) {
+						if (holiday.getName().equals(name)) {
 							contains = true;
-							if (holiday.getDate().compareTo(new Date(map.get("date"))) < 0 && holiday.getDate().isPast()) {
-								holiday.getParameter().replace("date", map.get("date"));
+							if (holiday.getDate().compareTo(newDate) < 0 && holiday.getDate().isPast()) {
+								holiday.getParameter().replace("date", date);
 								return;
 							}
 						}
 					}
-					if (!contains && new Date(map.get("date")).compareTo(Date.getCurrentDate()) >= 0) {
+					if (!contains && !newDate.isPast()) {
 						sortedAdd(new Holiday(map));
 					}
 				}
