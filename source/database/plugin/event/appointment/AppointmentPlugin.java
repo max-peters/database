62d1720b647df4477381027fe4d58d6273117a6c
Max Peters <de.maxptrs@googlemail.com>
2016-01-25 16:44:21 +0100

    update

--- source/database/plugin/event/appointment/AppointmentPlugin.java
+++ source/database/plugin/event/appointment/AppointmentPlugin.java
@@ -1,10 +1,23 @@
 package database.plugin.event.appointment;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.text.BadLocationException;
 import database.plugin.event.EventPluginExtention;
 import database.plugin.storage.Storage;
 
 public class AppointmentPlugin extends EventPluginExtention {
 	public AppointmentPlugin(Storage storage) {
 		super("appointment", new AppointmentList(), storage);
+	}
+
+	public void createRequest() throws InterruptedException, BadLocationException, IOException {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", ".*");
+		map.put("date", null);
+		request(map);
+		getInstanceList().add(map);
+		update();
 	}
 }
