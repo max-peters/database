2440eb11e13dc8912867e24267ae7eaa14b930f4
Maximilian Peters <uvdyr@student.kit.edu>
2016-04-17 23:10:30 +0200

    update

---
+++ source/database/plugin/settings/Settings.java
@@ -0,0 +1,11 @@
+package database.plugin.settings;
+
+import database.plugin.Plugin;
+
+public class Settings extends Plugin {
+	public Settings() {
+		super("settings");
+	}
+
+	private int displayedDays;
+}
