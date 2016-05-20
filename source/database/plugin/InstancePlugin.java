03bb80c34ee9bccf9679966d02ac58ae3787ebd9
Maximilian Peters <uvdyr@student.kit.edu>
2016-05-20 17:45:38 +0200

    update

--- source/database/plugin/InstancePlugin.java
+++ source/database/plugin/InstancePlugin.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import com.google.gson.Gson;
 import database.main.userInterface.StringFormat;
@@ -34,12 +33,6 @@ public abstract class InstancePlugin<T extends Instance> extends Plugin {
 
 	public void clearList() {
 		list.clear();
-	}
-
-	public abstract T create(NamedNodeMap nodeMap);
-
-	public void createAndAdd(NamedNodeMap nodeMap) throws ParserConfigurationException {
-		add(create(nodeMap));
 	}
 
 	public Iterable<T> getIterable() {
