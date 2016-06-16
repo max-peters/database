7a9efbacef250ad1ff9a2848a607cdeb3393a397
Max Peters <de.maxptrs@googlemail.com>
2016-06-16 18:25:34 +0200

    update

--- source/database/main/userInterface/StringUtility.java
+++ source/database/main/userInterface/StringUtility.java
@@ -6,13 +6,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class StringUtility {
-	private String fill(String string, int k) {
-		for (string.length(); string.length() < k;) {
-			string = "0" + string;
-		}
-		return string;
-	}
-
 	public String getElementWithDistance(String input, String[] splitResult, int levenshteinDistance) {
 		for (String string : splitResult) {
 			if (levenshteinDistance(input, string) <= levenshteinDistance) {
@@ -20,30 +13,6 @@ public class StringUtility {
 			}
 		}
 		return null;
-	}
-
-	private int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
-		int len0 = lhs.length() + 1;
-		int len1 = rhs.length() + 1;
-		int[] cost = new int[len0];
-		int[] newcost = new int[len0];
-		for (int i = 0; i < len0; i++) {
-			cost[i] = i;
-		}
-		for (int j = 1; j < len1; j++) {
-			newcost[0] = j;
-			for (int i = 1; i < len0; i++) {
-				int match = lhs.charAt(i - 1) == rhs.charAt(j - 1) ? 0 : 1;
-				int cost_replace = cost[i - 1] + match;
-				int cost_insert = cost[i] + 1;
-				int cost_delete = newcost[i - 1] + 1;
-				newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
-			}
-			int[] swap = cost;
-			cost = newcost;
-			newcost = swap;
-		}
-		return cost[len0 - 1];
 	}
 
 	public String parseDateString(String date) {
@@ -100,5 +69,36 @@ public class StringUtility {
 		catch (DateTimeParseException e) {
 			return false;
 		}
+	}
+
+	private String fill(String string, int k) {
+		for (string.length(); string.length() < k;) {
+			string = "0" + string;
+		}
+		return string;
+	}
+
+	private int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
+		int len0 = lhs.length() + 1;
+		int len1 = rhs.length() + 1;
+		int[] cost = new int[len0];
+		int[] newcost = new int[len0];
+		for (int i = 0; i < len0; i++) {
+			cost[i] = i;
+		}
+		for (int j = 1; j < len1; j++) {
+			newcost[0] = j;
+			for (int i = 1; i < len0; i++) {
+				int match = lhs.charAt(i - 1) == rhs.charAt(j - 1) ? 0 : 1;
+				int cost_replace = cost[i - 1] + match;
+				int cost_insert = cost[i] + 1;
+				int cost_delete = newcost[i - 1] + 1;
+				newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+			}
+			int[] swap = cost;
+			cost = newcost;
+			newcost = swap;
+		}
+		return cost[len0 - 1];
 	}
 }
