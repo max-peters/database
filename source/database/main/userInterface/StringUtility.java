6e35ec68f1c5756389ef23a64cebc823d2b62a78
Max Peters <de.maxptrs@googlemail.com>
2016-06-16 18:24:47 +0200

    update

---
+++ source/database/main/userInterface/StringUtility.java
@@ -0,0 +1,104 @@
+package database.main.userInterface;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class StringUtility {
+	private String fill(String string, int k) {
+		for (string.length(); string.length() < k;) {
+			string = "0" + string;
+		}
+		return string;
+	}
+
+	public String getElementWithDistance(String input, String[] splitResult, int levenshteinDistance) {
+		for (String string : splitResult) {
+			if (levenshteinDistance(input, string) <= levenshteinDistance) {
+				return string;
+			}
+		}
+		return null;
+	}
+
+	private int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
+		int len0 = lhs.length() + 1;
+		int len1 = rhs.length() + 1;
+		int[] cost = new int[len0];
+		int[] newcost = new int[len0];
+		for (int i = 0; i < len0; i++) {
+			cost[i] = i;
+		}
+		for (int j = 1; j < len1; j++) {
+			newcost[0] = j;
+			for (int i = 1; i < len0; i++) {
+				int match = lhs.charAt(i - 1) == rhs.charAt(j - 1) ? 0 : 1;
+				int cost_replace = cost[i - 1] + match;
+				int cost_insert = cost[i] + 1;
+				int cost_delete = newcost[i - 1] + 1;
+				newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+			}
+			int[] swap = cost;
+			cost = newcost;
+			newcost = swap;
+		}
+		return cost[len0 - 1];
+	}
+
+	public String parseDateString(String date) {
+		String formattedDate = null;
+		if (date.isEmpty()) {
+			formattedDate = "";
+		}
+		else {
+			String[] splitResult = date.split("\\.");
+			formattedDate = fill(splitResult[0], 2) + ".";
+			if (splitResult.length == 1) {
+				formattedDate = formattedDate + fill(String.valueOf(LocalDate.now().getMonthValue()), 2) + "." + fill(String.valueOf(LocalDate.now().getYear()), 4);
+			}
+			else if (splitResult.length == 2) {
+				formattedDate = formattedDate + fill(splitResult[1], 2) + "." + fill(String.valueOf(LocalDate.now().getYear()), 4);
+			}
+			else if (splitResult.length == 3) {
+				formattedDate = formattedDate + fill(splitResult[1], 2) + "." + fill(splitResult[2], 4);
+			}
+		}
+		return formattedDate;
+	}
+
+	public String parseTimeString(String time) {
+		String formattedTime = null;
+		String[] splitResult = time.split(":");
+		if (time.isEmpty()) {
+			formattedTime = "";
+		}
+		else if (splitResult.length == 1) {
+			formattedTime = fill(splitResult[0], 2) + ":00";
+		}
+		else if (splitResult.length == 2) {
+			formattedTime = fill(splitResult[0], 2) + ":" + fill(splitResult[1], 2);
+		}
+		return formattedTime;
+	}
+
+	public boolean testDateString(String date) {
+		try {
+			LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+			return true;
+		}
+		catch (DateTimeParseException e) {
+			return false;
+		}
+	}
+
+	public boolean testTimeString(String time) {
+		try {
+			LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+			return true;
+		}
+		catch (DateTimeParseException e) {
+			return false;
+		}
+	}
+}
--- source/database/main/userInterface/Terminal.java
+++ source/database/main/userInterface/Terminal.java
@@ -1,9 +1,5 @@
 package database.main.userInterface;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -19,11 +15,13 @@ public class Terminal {
 	private static Map<OutputInformation, String>	collectedLines;
 	private static GraphicalUserInterface			graphicalUserInterface;
 	private static PluginContainer					pluginContainer;
+	private static StringUtility					stringUtility;
 
 	public Terminal(GraphicalUserInterface graphicalUserInterface, PluginContainer pluginContainer) {
 		Terminal.graphicalUserInterface = graphicalUserInterface;
 		Terminal.pluginContainer = pluginContainer;
 		Terminal.collectedLines = new LinkedHashMap<OutputInformation, String>();
+		Terminal.stringUtility = new StringUtility();
 	}
 
 	public static void blockInput() {
@@ -98,7 +96,7 @@ public class Terminal {
 				waitForInput();
 			}
 			else {
-				result = getElementWithDistance(input, splitResult, levenshteinDistance);
+				result = stringUtility.getElementWithDistance(input, splitResult, levenshteinDistance);
 				if (result != null) {
 					request = false;
 				}
@@ -136,12 +134,12 @@ public class Terminal {
 				result = input;
 				request = false;
 			}
-			else if ("DATE".equals(regex) && (testDateString(parseDateString(input)) || input.isEmpty())) {
-				result = parseDateString(input);
+			else if ("DATE".equals(regex) && (stringUtility.testDateString(stringUtility.parseDateString(input)) || input.isEmpty())) {
+				result = stringUtility.parseDateString(input);
 				request = false;
 			}
-			else if ("TIME".equals(regex) && (testTimeString(parseTimeString(input)) || input.isEmpty())) {
-				result = parseTimeString(input);
+			else if ("TIME".equals(regex) && (stringUtility.testTimeString(stringUtility.parseTimeString(input)) || input.isEmpty())) {
+				result = stringUtility.parseTimeString(input);
 				request = false;
 			}
 			else {
@@ -161,102 +159,5 @@ public class Terminal {
 
 	public static void waitForInput() throws InterruptedException {
 		graphicalUserInterface.waitForInput();
-	}
-
-	private static String add(String string, int k) {
-		String s = string;
-		for (string.length(); s.length() < k;) {
-			s = "0" + s;
-		}
-		return s;
-	}
-
-	private static String getElementWithDistance(String input, String[] splitResult, int levenshteinDistance) {
-		for (String string : splitResult) {
-			if (levenshteinDistance(input, string) <= levenshteinDistance) {
-				return string;
-			}
-		}
-		return null;
-	}
-
-	private static int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
-		int len0 = lhs.length() + 1;
-		int len1 = rhs.length() + 1;
-		int[] cost = new int[len0];
-		int[] newcost = new int[len0];
-		for (int i = 0; i < len0; i++) {
-			cost[i] = i;
-		}
-		for (int j = 1; j < len1; j++) {
-			newcost[0] = j;
-			for (int i = 1; i < len0; i++) {
-				int match = lhs.charAt(i - 1) == rhs.charAt(j - 1) ? 0 : 1;
-				int cost_replace = cost[i - 1] + match;
-				int cost_insert = cost[i] + 1;
-				int cost_delete = newcost[i - 1] + 1;
-				newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
-			}
-			int[] swap = cost;
-			cost = newcost;
-			newcost = swap;
-		}
-		return cost[len0 - 1];
-	}
-
-	private static String parseDateString(String date) {
-		String formattedDate = null;
-		if (date.isEmpty()) {
-			formattedDate = "";
-		}
-		else {
-			String[] splitResult = date.split("\\.");
-			formattedDate = add(splitResult[0], 2) + ".";
-			if (splitResult.length == 1) {
-				formattedDate = formattedDate + add(String.valueOf(LocalDate.now().getMonthValue()), 2) + "." + add(String.valueOf(LocalDate.now().getYear()), 4);
-			}
-			else if (splitResult.length == 2) {
-				formattedDate = formattedDate + add(splitResult[1], 2) + "." + add(String.valueOf(LocalDate.now().getYear()), 4);
-			}
-			else if (splitResult.length == 3) {
-				formattedDate = formattedDate + add(splitResult[1], 2) + "." + add(splitResult[2], 4);
-			}
-		}
-		return formattedDate;
-	}
-
-	private static String parseTimeString(String time) {
-		String formattedTime = null;
-		String[] splitResult = time.split(":");
-		if (time.isEmpty()) {
-			formattedTime = "";
-		}
-		else if (splitResult.length == 1) {
-			formattedTime = add(splitResult[0], 2) + ":00";
-		}
-		else if (splitResult.length == 2) {
-			formattedTime = add(splitResult[0], 2) + ":" + add(splitResult[1], 2);
-		}
-		return formattedTime;
-	}
-
-	private static boolean testDateString(String date) {
-		try {
-			LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-			return true;
-		}
-		catch (DateTimeParseException e) {
-			return false;
-		}
-	}
-
-	private static boolean testTimeString(String time) {
-		try {
-			LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
-			return true;
-		}
-		catch (DateTimeParseException e) {
-			return false;
-		}
 	}
 }
