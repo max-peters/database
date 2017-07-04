9d1e2091e4a495094d17d12ba07fe90582d4f81a
Maximilian Peters <de.maxptrs@googlemail.com>
2017-07-04 09:56:16 +0200

    update

--- source/database/main/Main.java
+++ source/database/main/Main.java
@@ -56,7 +56,7 @@ public class Main {
 			Administration administration = new Administration();
 			databaseThread = new Thread(() -> {
 				try {
-					database.connect("jdbc:mysql://localhost/DATABASE?useSSL=false", "root", "vfr4");
+					database.connect("jdbc:mysql://localhost/DATABASE?useSSL=false", "root", "***REMOVED***");
 					logger.log("database connected");
 				}
 				catch (Exception e) {
