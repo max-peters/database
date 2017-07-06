6bcc663eb6dc06a7941cf977c2d0566091a647c8
Maximilian Peters <de.maxptrs@googlemail.com>
2017-07-06 14:53:30 +0200

    update

--- source/database/main/userInterface/RequestType.java
+++ source/database/main/userInterface/RequestType.java
@@ -1,13 +1,13 @@
 package database.main.userInterface;
 
 public class RequestType {
-	public static final String	BOOLEAN				= "(true|false)";
-	public static final String	DATE				= "DATE";
-	public static final String	DOUBLE				= "(\\-)?[0-9]{1,13}(\\.[0-9]{1,2})?";
-	public static final String	EXECUTIONDAY		= "(first|mid|last)";
-	public static final String	INTEGER				= "(\\-)?[0-9]{1,8}";
-	public static final String	NAME				= "[A-Za-zÖöÄäÜüß\\-\\.,:][A-Za-zÖöÄäÜüß\\-\\.,: ]*";
-	public static final String	NAME_NUMBER			= "[A-Za-zÖöÄäÜüß0-9\\-\\.,:()][A-Za-zÖöÄäÜüß0-9\\-\\.,:() ]*";
-	public static final String	NAME_NUMBER_EMPTY	= "([A-Za-zÖöÄäÜüß0-9\\-\\.,:()][A-Za-zÖöÄäÜüß0-9\\-\\.,:() ]*|)";
-	public static final String	TIME				= "TIME";
+	public static final String BOOLEAN = "(true|false)";
+	public static final String DATE = "DATE";
+	public static final String DOUBLE = "(\\-)?[0-9]{1,13}(\\.[0-9]{1,2})?";
+	public static final String EXECUTIONDAY = "(first|mid|last)";
+	public static final String INTEGER = "(\\-)?[0-9]{1,8}";
+	public static final String NAME = "[A-Za-z\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,:][A-Za-z\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,: ]*";
+	public static final String NAME_NUMBER = "[A-Za-z0-9\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,:()][A-Za-z0-9\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,:() ]*";
+	public static final String NAME_NUMBER_EMPTY = "([A-Za-z0-9\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,:()][A-Za-z0-9\u00C4\u00E4\u00D6\u00F6\u00DC\u00FC\u00DF\\-\\.,:() ]*|)";
+	public static final String TIME = "TIME";
 }
